4dc910d7a9f64d2245149d2c269687a76afbb232
umutyildiz <56760679+umutyildiz1@users.noreply.github.com>
2021-06-04 19:46:59 +0300

    Job Post added

--- hrms/src/main/java/umutyildiz/hrms/api/controller/JobsController.java
+++ hrms/src/main/java/umutyildiz/hrms/api/controller/JobsController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import umutyildiz.hrms.business.abstracts.JobService;
 import umutyildiz.hrms.core.utilities.results.DataResult;
+import umutyildiz.hrms.core.utilities.results.Result;
 import umutyildiz.hrms.entities.concretes.Job;
 
 @RestController
@@ -20,5 +23,10 @@ public class JobsController {
 	@GetMapping("/getall")
 	public DataResult<List<Job>> getAll(){
 		return this.jobService.getAll();
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Job job) {
+		return this.jobService.add(job);
 	}
 }
--- hrms/src/main/java/umutyildiz/hrms/business/concretes/JobManager.java
+++ hrms/src/main/java/umutyildiz/hrms/business/concretes/JobManager.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import umutyildiz.hrms.business.abstracts.JobService;
 import umutyildiz.hrms.core.utilities.results.DataResult;
+import umutyildiz.hrms.core.utilities.results.Result;
 import umutyildiz.hrms.core.utilities.results.SuccessDataResult;
+import umutyildiz.hrms.core.utilities.results.SuccessResult;
 import umutyildiz.hrms.dataAccess.abstracts.JobDao;
 import umutyildiz.hrms.entities.concretes.Job;
 
@@ -19,6 +21,12 @@ public class JobManager implements JobService{
 	@Override
 	public DataResult<List<Job>> getAll() {
 		return new SuccessDataResult<List<Job>>(this.jobDao.findAll(),"Job titles Listelendi");
+	}
+
+	@Override
+	public Result add(Job job) {
+		this.jobDao.save(job);
+		return new SuccessResult("Job title eklendi");
 	}
 
 }
